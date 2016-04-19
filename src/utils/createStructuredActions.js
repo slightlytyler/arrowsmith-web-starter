5844f92f7e7537db21c56e6e43b41113fcf5989e
Tyler Martinez <slightlytyler@gmail.com>
2016-04-18 19:53:42 -0500

    createStructuredActions only uses per-instance memoization if propKeys are specified

--- src/utils/createStructuredActions.js
+++ src/utils/createStructuredActions.js
@@ -5,11 +5,13 @@ const createPartials = (actions, args) => (
   mapValues(actions, action => partial(action, ...args))
 );
 
-export default (actions, ...propKeys) => (dispatch, props) => () => {
+export default (actions, ...propKeys) => {
   if (propKeys.length) {
-    const args = propKeys.map(key => props[key]);
-    return bindActionCreators(createPartials(actions, args), dispatch);
+    return (dispatch, props) => () => {
+      const args = propKeys.map(key => props[key]);
+      return bindActionCreators(createPartials(actions, args), dispatch);
+    };
   }
 
-  return bindActionCreators(actions, dispatch);
-};
+  return dispatch => bindActionCreators(actions, dispatch);
+}
