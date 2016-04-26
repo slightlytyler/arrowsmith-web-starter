bc0529d4f58d2f74f1fac4143ff60623beff664c
Tyler Martinez <slightlytyler@gmail.com>
2016-04-25 21:07:37 -0500

    getModuleAtoms uses reduce

--- src/utils/getModuleAtoms.js
+++ src/utils/getModuleAtoms.js
@@ -1,5 +1,9 @@
-import { map, flatten } from 'lodash';
+import { reduce } from 'lodash';
 
 // Takes set of modules and the atom key i.e. hooks
 // Returns an array of all the module atoms i.e. all the hooks
-export default (modules, key) => flatten(map(modules, m => m[key] ? Object.values(m[key]) : []));
+export default (modules, key) => reduce(
+  modules,
+  (result, m) => m[key] ? [...result, ...Object.values(m[key])] : result,
+  []
+);
